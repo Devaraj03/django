8cebf68374d3968dc64626d7f908236aa7f22777
Devaraj S <115549688+Devaraj03@users.noreply.github.com>
2024-05-06 21:04:49 +0530

    Delete 3.10 directory

--- 3.10/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from blog.views import indexes
-from django.urls import path,include
-
-urlpatterns = [
-    path('',indexes),
-]
--- 3.10/blog/views.py
+++
@@ -1,5 +0,0 @@
-from blog.urls import *
-from django.shortcuts import render
-
-def indexes(request):
-    return render(request,'index.html')
